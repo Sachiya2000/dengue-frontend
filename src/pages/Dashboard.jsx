e0921ec180eaaa83c6bcc0d0ed634c8a0e3cb76a
Sachitha Madhuranga <118953518+Sachiya2000@users.noreply.github.com>
2025-02-10 08:56:07 +0530

    update

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,9 +5,7 @@ import DenguePredictionDashboard from "../components/DenguePredictionForm";
 const Dashboard = () => {
   return (
     <div>
-      <h1 className="text-2xl font-bold text-center my-6">
-        Dengue Cases Prediction System
-      </h1>
+     
       <DenguePredictionDashboard />
     </div>
   );
